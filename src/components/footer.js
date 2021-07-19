71b15f1c045b24c3b7311fef68f2537fe93deaf1
Wesley Cintra do Nascimento <cintra.wesley@gmail.com>
2021-07-19 00:03:39 -0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ export const footer = (
 			{'Copyright © '}
 			<Link color="inherit" href="https://valorant-wiki.herokuapp.com/">Valorant-Wiki</Link>{' '}{new Date().getFullYear()}{'.'}
 		</Typography>
-		<Typography variant="body2" color="textSecondary" align="center">Valorant-API is a non-official API and not endorsed by Riot Games in any way. </Typography>
+		<Typography variant="body2" color="textSecondary" align="center">Valorant-Wiki is a non-official API and not endorsed by Riot Games in any way. </Typography>
 		<Typography variant="body2" color="textSecondary" align="center">Riot Games, Valorant, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</Typography>
 		<Typography variant="body2" color="textSecondary" align="center">Content is provided by valorant-api.com with ❤️</Typography>
 	</div>
